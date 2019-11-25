749a313819eac00e532fb1ff4bd59822f8fce7f9
Yen Yin Hing <yenyinf287@gmail.com>
2019-11-24 20:34:53 -0800

    implement project structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from "react";
+import ReactDOM from "react-dom";
+import { BtnProvider } from "./Context/BtnContext";
+import ImageBtn from "./Components/ImageBtn";
+import QuoteBtn from "./Components/QuoteButton";
+import Image from "./Components/Image";
+import Quote from "./Components/Quote";
+const App = () => {
+  return (
+    <div>
+      <BtnProvider>
+        <div>
+          <Image />
+          <ImageBtn />
+          <Quote />
+          <QuoteBtn />
+        </div>
+      </BtnProvider>
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.querySelector("#root"));
