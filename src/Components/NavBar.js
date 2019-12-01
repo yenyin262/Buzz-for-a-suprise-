931db23c05b370beb87a45315ae236b104a81692
Yen Yin Hing <yenyinf287@gmail.com>
2019-11-30 16:21:11 -0800

    add NavBar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import AppBar from "@material-ui/core/AppBar";
+import React from "react";
+import style from "./NavBar.module.css";
+const NavBar = () => {
+  return (
+    <AppBar position="static" className={style.navbar}>
+      <p style={{ color: "white", fontSize: "32px", margin: "0px auto" }}>
+        Buzz for a Suprise!
+      </p>
+    </AppBar>
+  );
+};
+
+export default NavBar;
