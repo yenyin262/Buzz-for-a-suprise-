ca83b2dea2a1359a7e62912003ca06976a2d3e3d
Yen Yin Hing <yenyinf287@gmail.com>
2020-01-09 22:53:22 -0800

    style navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,7 +4,7 @@ import style from "./NavBar.module.css";
 const NavBar = () => {
   return (
     <AppBar position="static" className={style.navbar}>
-      <p style={{ color: "white", fontSize: "32px", margin: "0px auto" }}>
+      <p style={{ color: "white", fontSize: "32px", margin: "20px auto" }}>
         Buzz for a Suprise!
       </p>
     </AppBar>
